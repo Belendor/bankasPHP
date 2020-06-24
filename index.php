5d8c838aa322a25b77f93eb1315600d8bb80280d
Belendor <dwarf11@gmail.com>
2020-06-24 13:09:29 +0300

    index

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-echo "pradeti naudotis";
-echo '<a href="/saskaitu-sarasas.php">Perziureti saskaitu sarasa <i class="text-icon icon-external-link"></i></a><br>';
+
+header('Location: /login.php');
+die();
